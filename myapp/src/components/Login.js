bfa174a8d38c7c3a5cd9cda8df8eaef628ea5c1d
Chesintha <139671816+Chesintha@users.noreply.github.com>
2024-11-29 11:12:05 +0000

    Login page updated

---
+++ myapp/src/components/Login.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const navigate = useNavigate();
+    const[user, setuser] = useState({username:"", pwd:"", error:""})
+    const getTextInput =(e)=>{
+        const{name,value}=e.target;
+        setuser((prevUser)=>({
+            ...prevUser,[name]:value,
+        }))
+    }
+    const authenticate = () => {
+        if(user.username === "Admin" && user.pwd === "abc@123"){
+           setuser({username:"", pwd:"", error:""}) 
+           navigate('/dash');
+        }
+        else{
+            setuser({'error':"Please chech your username and password !"})
+        }
+    }
+    return(
+        <div>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="username" onChange={getTextInput}/> </td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" name="pwd" onChange={getTextInput}/></td>
+                </tr>
+                <tr>
+                    <td style={{color: 'red'}} colSpan={2}>{user.error}</td>
+                </tr>
+            </table>
+            <button onClick={aunthenticate}>Login</button>
+        </div>
+    );
+}
